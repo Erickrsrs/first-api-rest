3f367b10ecabe9ab236e7af97b7a3315adfb4a04
Erick Rocha <erick2003rocha@gmail.com>
2022-06-23 15:16:08 -0300

    fix: fixing find book by id route

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -7,7 +7,7 @@ router
   .get("/livros", LivroController.listarLivros)
   .get("/livros/busca/editora", LivroController.listarLivroPorEditora)
   .get("/livros/busca/titulo", LivroController.listarLivroPorTitulo)
-  .get("/livros/:id", LivroController.listarLivroPorId)
+  .get("/livros/busca/:id", LivroController.listarLivroPorId)
   .post("/livros", LivroController.cadastrarLivro)
   .put("/livros/:id", LivroController.atualizarLivro)
   .delete("/livros/:id", LivroController.excluirLivro);
